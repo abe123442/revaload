7e78ddb4785362c23e83d06f18d34d4b87c3475f
Abraham Assariparambil Earnest <z5481911@ad.unsw.edu.au>
2024-09-02 22:36:07 +1000

    Updated latest

--- src/collections/Books/index.ts
+++ src/collections/Books/index.ts
@@ -1,6 +1,5 @@
-import { revalidateTag } from 'next/cache'
 import type { CollectionConfig } from 'payload'
-import { revalidateBooks } from './Books/revalidateBooks'
+import { revalidateBooks } from './revalidateBooks'
 
 export const Books: CollectionConfig = {
   slug: 'books',
